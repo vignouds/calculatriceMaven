e3c430a48dfc69b18157beefc4b4988c5b2c398c
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-12 17:33:54 +0200

    creation Class main avec fonction mail et classe Calculatrice avec contructeur et 1 fonction par operateur (vides)

---
+++ src/main/java/Calculatrice.java
@@ -0,0 +1,22 @@
+
+public class Calculatrice {
+	public Calculatrice(){
+		
+	}
+	
+	public void	additioner() {
+		
+	}
+	
+	public void	soustraire() {
+		
+	}
+	
+	public void	multiplier() {
+		
+	}
+	
+	public void	diviser() {
+		
+	}
+}
